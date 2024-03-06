b438847ae2089038f18c9f5fff6b8f8735e6dee6
Hiten Jaypal <100457414+hitenjaypal@users.noreply.github.com>
2024-03-07 00:39:17 +0530

    Add files via upload

---
+++ JS Codes/Lec 7/events.js
@@ -0,0 +1,34 @@
+// The change in the state of an object is known as an Event
+// An event can be triggered by a user action
+// Events are fired to notify code of "interesting changes" that may affect code execution.
+
+// Mouse events (click, double click etc.)
+// Keyboard events (keypress, keyup, keydown)
+// Form events (submit etc.)
+// Print event & many more
+
+
+// <<<<<<<<<READ GIT REPO FOR MORE KNOWLDEGE >>>>>>>>>>>
+
+
+
+// 2. 
+node.event = ( ) => {
+    //handle here
+    }
+
+    btn.onclick = ( ) => {
+    console.log(“btn was clicked”);
+    }
+
+
+// 3.    Event Listeners
+
+// node.addEventListener( event, callback )
+
+// node.removeEventListener( event, callback )
+
+// *Note : the callback reference should be same to remove
+
+
+// WATCH THAT LECTURE IF HAVE ANY QUERIES IN FUT
